79e936c008aaf29bd71a982f522e7673c168dd2e
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-10-06 22:00:23 +0200

    Refactor device diagnostics tests to make screenshot verification mandatory

--- app-runner/Tests/Device.Tests.ps1
+++ app-runner/Tests/Device.Tests.ps1
@@ -337,7 +337,6 @@ Describe '<TargetName>' -Tag 'RequiresDevice' -ForEach $TestTargets {
             $fileNames = $files | Select-Object -ExpandProperty Name
             $fileNames | Where-Object { $_ -like '*-device-status.json' } | Should -Not -BeNullOrEmpty
             $fileNames | Where-Object { $_ -like '*-system-info.txt' } | Should -Not -BeNullOrEmpty
-            # Screenshot may fail with TestDrive: path on some platforms, so it's optional
 
             # Verify device status file has valid JSON
             $statusFile = $files | Where-Object { $_.Name -like '*-device-status.json' } | Select-Object -First 1
@@ -345,9 +344,7 @@ Describe '<TargetName>' -Tag 'RequiresDevice' -ForEach $TestTargets {
 
             # Verify screenshot file if it exists (optional, may fail with TestDrive paths)
             $screenshotFile = $files | Where-Object { $_.Name -like '*-screenshot.png' } | Select-Object -First 1
-            if ($screenshotFile) {
-                $screenshotFile.Length | Should -BeGreaterThan 0
-            }
+            $screenshotFile.Length | Should -BeGreaterThan 0
 
             # Verify system info file has content
             $sysInfoFile = $files | Where-Object { $_.Name -like '*-system-info.txt' } | Select-Object -First 1
