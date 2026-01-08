2d1f244e39953965d3701a29b21b6383da2d15be
Serhii Snitsaruk <limbonaut@pm.me>
2026-01-08 20:19:57 +0100

    Add iOS template app

---
+++ app-runner/Tests/Fixtures/iOS/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Serhii Snitsaruk on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
